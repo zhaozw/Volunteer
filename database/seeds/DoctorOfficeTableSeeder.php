ead7f8bec9db8fd123534c8f42185dbb23d577a0
蔡乾 <cai.q@foxmail.com>
2015-11-27 11:56:34 +0800

    beans页面调整。

--- database/seeds/DoctorOfficeTableSeeder.php
+++ database/seeds/DoctorOfficeTableSeeder.php
@@ -12,5 +12,24 @@ class DoctorOfficeTableSeeder extends Seeder
     public function run()
     {
         //
+        DB::table('doctor_offices')->insert(
+            ['name' => '内分泌科']
+        );
+
+        DB::table('doctor_offices')->insert(
+            ['name' => '综合内科']
+        );
+
+        DB::table('doctor_offices')->insert(
+            ['name' => '外科（甲状腺）']
+        );
+
+        DB::table('doctor_offices')->insert(
+            ['name' => '核医学科']
+        );
+
+        DB::table('doctor_offices')->insert(
+            ['name' => '其他科室']
+        );
     }
 }
