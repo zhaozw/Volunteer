c48b07c564739a25ab84b13c87d131864874dad4
Icefly <501254061@qq.com>
2015-12-11 22:28:31 +0800

    黄埔学堂html页面

---
+++ resources/views/volunteer/huangpu_ppt.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable=no">
+
+  <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+  <link rel="stylesheet" href="/css/huangpu_index.css">
+</head>
+<body class="huangpu-body">
+<div class="huangpu-pic"><img src="/image/huangpu_ppt/ppt1.png" alt="ppt1" class="img-responsive center-block"></div>
+<div class="huangpu-pic"><img src="/image/huangpu_ppt/ppt2.png" alt="ppt2" class="img-responsive center-block"></div>
+<div class="huangpu-pic"><img src="/image/huangpu_ppt/ppt3.png" alt="ppt3" class="img-responsive center-block"></div>
+<div class="huangpu-pic"><img src="/image/huangpu_ppt/ppt4.png" alt="ppt4" class="img-responsive center-block"></div>
+<div class="huangpu-pic"><img src="/image/huangpu_ppt/ppt5.png" alt="ppt5" class="img-responsive center-block"></div>
+<div class="huangpu-pic"><img src="/image/huangpu_ppt/ppt6.png" alt="ppt6" class="img-responsive center-block"></div>
+<div class="huangpu-pic"><img src="/image/huangpu_ppt/ppt7.png" alt="ppt7" class="img-responsive center-block"></div>
+<div class="huangpu-pic"><img src="/image/huangpu_ppt/ppt8.png" alt="ppt8" class="img-responsive center-block"></div>
+<div class="huangpu-pic"><img src="/image/huangpu_ppt/ppt9.png" alt="ppt9" class="img-responsive center-block"></div>
+<div class="huangpu-pic"><img src="/image/huangpu_ppt/ppt10.png" alt="ppt10" class="img-responsive center-block"></div>
+<div class="huangpu-pic"><img src="/image/huangpu_ppt/ppt11.png" alt="ppt11" class="img-responsive center-block"></div>
+<div class="huangpu-pic"><img src="/image/huangpu_ppt/ppt12.png" alt="ppt12" class="img-responsive center-block"></div>
+<div class="huangpu-pic"><img src="/image/huangpu_ppt/ppt13.png" alt="ppt13" class="img-responsive center-block"></div>
+<div class="huangpu-pic"><img src="/image/huangpu_ppt/ppt14.png" alt="ppt14" class="img-responsive center-block"></div>
+<div class="huangpu-pic"><img src="/image/huangpu_ppt/ppt15.png" alt="ppt15" class="img-responsive center-block"></div>
+</body>
+</html>
---
+++ resources/views/volunteer/huangpu_profile.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title> </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable=no">
+  <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/css/huangpu_index.css">
+</head>
+<body>
+  <div class="container">
+    <h1>项目简介</h1>
+    <hr>
+    <p>2016年计划在全国16个城市，开展基于内分泌进修医生下班后（黄埔夜校）活动打造线下、线下相结合的高端学术服务平台，以帮助内分泌进修医生提高临床治疗水平。项目主要形式为邀请进修医生所在城市知名省级专家轮流作为讲者，针对进修医生感兴趣的专题，进行学术授课。</p>
+    <hr>
+    <h3>线下部分活动介绍：</h3>
+    <ol>
+      <li>现场学术授课。</li>
+      <li>与全科医学协作平台-远程移动教育“医师助手”软件服务商合作，建立线上“黄埔学堂”学习班级。</li>
+     </ol>
+    <hr>
+    <h3>线上部分介绍：</h3>
+    <ol>
+      <li>进修医生在手机上安装、注册并加入线上“黄埔导师进修班”后，可国内大专家进行互动交流学习。</li>
+      <li>医生也可以将实际工作中遇的问题、病例上传至在线学习班，与同行在线交流。</li>
+      <li>每个班级都将配备一名班级助教，以帮助大家解决日常班务活动外，定期还会将班级内的热点问题收集整理为“问答集”交联系导师进行回复。</li>
+      <li>班级导师也将定期挑选优秀案例开展专题讲座。所有班级学员都可以参与导师的直播或录播授课学习。</li>
+    </ol>
+    <hr>
+    <h3>项目目的：</h3>
+    <p>旨在建立线上和线下相结合的高端学术服务平台，以帮助内分泌进修医生提高 临床治疗水平。</p>
+    <hr>
+    <h3 id="huangpu_flow"><br>项目流程</h3>
+    <img class="huangpu-pic" src="/image/huangpu_flow.png" alt="项目流程">
+    <br><br><br>
+  </div>
+</body>
+</html>
