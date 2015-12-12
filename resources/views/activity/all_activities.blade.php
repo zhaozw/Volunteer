8a42ecd9240e567903b00dcadcd3f5c107c1a277
PythonCakeMing <1851464029@qq.com>
2015-12-12 21:27:52 +0800

    xsm, fix title for activities

--- resources/views/activity/all_activities.blade.php
+++ resources/views/activity/all_activities.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>项目表格</title>
+    <title>我的活动</title>
 
     <link href="//cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
     <script src="//cdn.bootcss.com/jquery/2.1.4/jquery.min.js"></script>
