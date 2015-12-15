fb0404e95e00335402cf29d023d95bfb4d4deede
Icefly <501254061@qq.com>
2015-12-15 17:41:24 +0800

    bug-fix

--- app/Http/Controllers/HPXTController.php
+++ app/Http/Controllers/HPXTController.php
@@ -58,7 +58,7 @@ class HPXTController extends Controller
         $volunteer = Volunteer::where('openid', $user['openid'])->first();
 //        dd($volunteer);
         $classes = HPXTClass::where('volunteer_id', $volunteer->id)->get();
-        dd($classes);
+//        dd($classes);
         return view('activity.hpxt.class_manage')->with([
             'classes' => $classes
         ]);
