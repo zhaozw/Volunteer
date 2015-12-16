c929810ad0b462884c62fcef8e388eaee0e06160
Icefly <501254061@qq.com>
2015-12-16 11:14:50 +0800

    bug-fix

--- resources/views/activity/hpxt/class_application.blade.php
+++ resources/views/activity/hpxt/class_application.blade.php
@@ -8,9 +8,106 @@
     <link rel="stylesheet" href="/css/huangpu_index.css">
 </head>
 <body>
-
+@if (count($errors) > 0)
+    <div class="alert alert-danger">
+        <strong>错误！</strong> 请检查您的输入。<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 <div class="container">
-    <h1>meiyouwenti</h1>
+    <br>
+
+    <form action="{{url('/hpxt/class-store')}}" class="form-horizontal" method="post">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+        <div class="form-group">
+            <label class="form-label col-xs-4">班级名称</label>
+
+            <div class="col-xs-8">
+                <input type="text" class="form-control" name="name" value="{{old('name')}}">
+            </div>
+        </div>
+        <hr style="">
+        <div class="form-group">
+            <label class="form-label col-xs-4">班级规模</label>
+
+            <div class="col-xs-8">
+                <select class="form-control" name="{{old('scale_id')}}">
+                    @foreach($classsSales as $classsSale)
+                        <option value="{{$classsSale->id}}">{{$classsSale->scale}}</option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+        <hr>
+        <div class="form-group">
+            <label class="form-label col-xs-4">班级形式</label>
+
+            <div class="col-xs-8">
+                <select class="form-control" name="{{old('mode_id')}}">
+                    @foreach($classModes as $classMode)
+                        <option value="{{$classMode->id}}">{{$classMode->mode}}</option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+        <hr>
+        <div class="form-group">
+            <label class="form-label col-xs-4">招募时间</label>
+
+            <div class="col-xs-8">
+                <input type="date" class="form-control inputdate" name="time" value="{{old('time')}}">
+            </div>
+        </div>
+        <hr>
+        <div><label class="form-label">班级导师</label><span class="float-right">点击添加导师</span></div>
+        <div>
+            <div class="media-left media-middle">
+                <img class="docpic" src="/image/icons/private.png" alt="...">
+            </div>
+            <div class="media-body">
+                <br>
+                <h4 class="media-heading">某某某&nbsp;
+                    <small>主治医师</small>
+                </h4>
+                <p>中南医院呼吸科</p>
+            </div>
+        </div>
+        <div>
+            <div class="media-left media-middle">
+                <img class="docpic" src="/image/icons/private.png" alt="...">
+            </div>
+            <div class="media-body">
+                <br>
+                <h4 class="media-heading">某某某&nbsp;
+                    <small>主治医师</small>
+                </h4>
+                <p>中南医院呼吸科</p>
+            </div>
+        </div>
+        <hr>
+        <div><label class="form-label">班级助教</label><span class="float-right">点击添加助教</span></div>
+        <div>
+            <div class="media-left media-middle">
+                <img class="docpic" src="/image/icons/private.png" alt="...">
+            </div>
+            <div class="media-body">
+                <br>
+                <h4 class="media-heading">某某某&nbsp;
+                    <small>主治医师</small>
+                </h4>
+                <p>中南医院呼吸科</p>
+            </div>
+        </div>
+        <hr>
+        <button class="btn btn-block btn-primary btn-lg" type="submit">提交申请</button>
+    </form>
+
+    <br>
 </div>
 
 
