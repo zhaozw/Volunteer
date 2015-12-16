63b78ed81480d950dbea0833d49423aa5a27a3cb
Icefly <501254061@qq.com>
2015-12-16 11:10:52 +0800

    bug-fix

---
+++ resources/views/activity/hpxt/class_application222.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>申请班级</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable=no">
+    <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/huangpu_index.css">
+</head>
+<body>
+
+<div class="container">
+  <h1>nihao </h1>
+</div>
+
+
+<script src="https://cdn.bootcss.com/jquery/2.1.4/jquery.min.js"></script>
+<script src="http://cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body>
+</html>
