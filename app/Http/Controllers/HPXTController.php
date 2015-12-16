ea0fa2e2fa4b31580571b39c996990ef48683cf2
Icefly <501254061@qq.com>
2015-12-16 09:43:33 +0800

    bug-fix

--- app/Http/Controllers/HPXTController.php
+++ app/Http/Controllers/HPXTController.php
@@ -8,8 +8,6 @@ use Illuminate\Http\Request;
 
 use App\Model\Volunteer;
 use App\Model\HPXTClass;
-use App\Model\HPXTClassMode;
-use App\Model\HPXTClassScale;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
