45d1ffaed25aec5b05a4bee091d80fa3248b17f6
Icefly <501254061@qq.com>
2015-12-17 17:11:33 +0800

    迈徳志愿者改为医学志愿者

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>迈德志愿者</title>
+    <title>医学志愿者</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable=no">
 
     <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
