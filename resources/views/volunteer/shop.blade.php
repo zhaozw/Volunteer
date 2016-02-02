8ee02c7f7ceadc03ac9f849af63f94010aeb8b45
amudogxxx <amudog_1@hotmail.com>
2016-02-02 17:05:40 +0800

    增加view内容

--- resources/views/volunteer/shop.blade.php
+++ resources/views/volunteer/shop.blade.php
@@ -1,7 +1,16 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ming
- * Date: 2016/2/2
- * Time: 10:23
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>迈豆商城</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable=no">
+
+    <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="/css/user_point.css">
+
+</head>
+<body>
+<p>迈豆商城</p>
+</body>
+</html>
--- resources/views/volunteer/success.blade.php
+++ resources/views/volunteer/success.blade.php
@@ -1,7 +1,16 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ming
- * Date: 2016/2/1
- * Time: 12:52
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>个人信息</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable=no">
+
+    <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="/css/user_point.css">
+
+</head>
+<body>
+<p>注册成功</p>
+</body>
+</html>
