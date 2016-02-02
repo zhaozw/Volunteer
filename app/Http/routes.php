fd2839b01d5cd1bfd24b0e0ca498e41b47ae3373
amudogxxx <amudog_1@hotmail.com>
2016-02-02 15:36:22 +0800

    add route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -32,6 +32,8 @@ Route::group(['prefix' => 'volunteer'], function () {
     Route::post('update-self', 'VolunteerController@updateSelf');
 
     Route::get('beans', 'VolunteerController@beans');
+    Route::get('shop', 'VolunteerController@shop');
+    Route::get('about', 'VolunteerController@about');
 });
 
 
