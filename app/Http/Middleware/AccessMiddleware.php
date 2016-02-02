755e80f6f78586898bc3fa2874325ae97697d361
amudogxxx <amudog_1@hotmail.com>
2016-02-02 14:09:51 +0800

    add log

--- app/Http/Middleware/AccessMiddleware.php
+++ app/Http/Middleware/AccessMiddleware.php
@@ -26,10 +26,13 @@ class AccessMiddleware
         \Log::info('AccessMiddleware.openid:'.$user['openid']);
         $volunteer = Volunteer::where('openid', $user['openid'])->first();
         if (!$volunteer) {
+            \Log::info('AccessMiddleware.$volunteer: no data');
             return redirect('/volunteer/create-self');
         } /*if>*/
 
+        \Log::info('AccessMiddleware.diffInMinutes');
         if (Carbon::now()->diffInMinutes($volunteer->updated_at) > 30) {
+            \Log::info('AccessMiddleware.diffInMinutes > 30');
             $volunteer->openid      = $user['openid'];
             $volunteer->headimgurl  = $user['headimgurl'];
             $volunteer->nickname    = $user['nickname'];
