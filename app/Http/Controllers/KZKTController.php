179799281ea163150af516910f49820d84c60b09
amudogxxx <amudog_1@hotmail.com>
2016-02-03 16:07:41 +0800

    fix

--- app/Http/Controllers/KZKTController.php
+++ app/Http/Controllers/KZKTController.php
@@ -66,7 +66,7 @@ class KZKTController extends Controller
     function addClassroom(Request $request)
     {
         $openid = \Session::get('logged_user');
-        $volunteerId = 1;
+        $volunteerId = 0;
         if ($openid) {
             $volunteer = Volunteer::where('openid', $openid['openid'])->first();
             $volunteerId = $volunteer->id;
