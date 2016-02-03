6f735e998cd3ab90c48a809354d3ab24bb4009fc
amudogxxx <amudog_1@hotmail.com>
2016-02-03 15:28:01 +0800

    fix sql

--- app/Http/Controllers/KZKTController.php
+++ app/Http/Controllers/KZKTController.php
@@ -53,7 +53,7 @@ class KZKTController extends Controller
     {
         $id = $request->input('id');
         $list = DB::select('select id,hospital from hospitals where country_id= :id
-              order by hosptial_id', ['id' => $id]);
+              order by hospital_id', ['id' => $id]);
         return response()->json(['list'=>$list]);
     }
 
