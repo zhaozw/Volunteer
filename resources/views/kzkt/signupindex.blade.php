00c90b9114d97c2fe07dcc5e1a5590a7c02929f3
amudogxxx <amudog_1@hotmail.com>
2016-02-17 19:03:59 +0800

    modify signupindex

--- resources/views/kzkt/signupindex.blade.php
+++ resources/views/kzkt/signupindex.blade.php
@@ -1,23 +1,88 @@
+{{--<!DOCTYPE html>--}}
+{{--<html lang="en">--}}
+{{--<head>--}}
+    {{--<meta charset="UTF-8">--}}
+    {{--<meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">--}}
+    {{--<title>空中课堂报名</title>--}}
+
+    {{--<link rel="stylesheet" href="/css/weui.min.css">--}}
+    {{--<link rel="stylesheet" href="/css/volunteer.css">--}}
+    {{--<script type="application/javascript">--}}
+        {{--function goSignup() {--}}
+            {{--window.location.href = '/kzkt/signup';--}}
+        {{--}--}}
+    {{--</script>--}}
+{{--</head>--}}
+{{--<body>--}}
+{{--<div class="container index_pic">--}}
+    {{--<img src="/image/kzkt/mainpage.png" alt="">--}}
+    {{--<a type="button" class="weui_btn" onclick="goSignup()" >报&emsp;名</a>--}}
+{{--</div>--}}
+
+{{--</body>--}}
+{{--</html>--}}
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
     <title>空中课堂报名</title>
+    <link rel="stylesheet" href="/css/medtech.css">
 
-    <link rel="stylesheet" href="/css/weui.min.css">
-    <link rel="stylesheet" href="/css/volunteer.css">
-    <script type="application/javascript">
-        function goSignup() {
-            window.location.href = '/kzkt/signup';
-        }
-    </script>
 </head>
 <body>
-<div class="container index_pic">
-    <img src="/image/kzkt/mainpage.png" alt="">
-    <a type="button" class="weui_btn" onclick="goSignup()" >报&emsp;名</a>
-</div>
+    <div class="row" id="airclass">
+        <div class="small-10 small-centered large-4 large-centered columns">
+            <h4 class="text-center">课程简介</h4>
+            <h6 class="text-left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;全科医学协作平台远程教育项目-2016空中课堂内分泌代谢基础班、高级版、精品班由蓝海联盟（北京）医学研究院主办，由内分泌代谢专家结合基层常见的内分泌疾病诊疗误区和难点、设置的远程教育课程。</h6>
+            <div>
+                <div>
+                    <p>
+                        <img src="/image/airclass/u20.png" style="margin-left: -36px;">
+                        <span class="baoming">报名时间</span> <span class="time">2016年3月---2016年4月</span>
+                    </p>
+                    <p>
+                        <span class="baoming">开课时间</span> <span class="time">2016年5月---2016年11月</span>
+                    </p>
+                </div>
+            </div>
+            <div style="background-color: #007aff;height: 2px;width: auto"></div>
+            <div>
+
+                <p class="baoming"><img src="/image/airclass/u26.png">报名方式</p>
+                <a href="#" class="btn">志愿者端</a>
+                <a href="#" class="btn" style="background: #F96EA3">医生端</a>
+            </div>
+            <div>
+                <hr>
+                <p class="bj">
+                    <span class="banji">内分泌代谢疾病基础班</span><br>
+                    <span class="xiangqing">课程内容设计糖尿病的基础治疗手段和防治诊疗、以及降糖药的选择与延展等。</span>
+                    <a><img src="/image/airclass/u42.png"></a>
+                </p>
+
+                <hr>
+                <p class="bj">
+                    <span class="banji">内分泌代谢疾病基础班</span><br>
+                    <span class="xiangqing">课程内容设计糖尿病的基础治疗手段和防治诊疗、以及降糖药的选择与延展等。</span>
+                    <a><img src="/image/airclass/u42.png"></a>
+                </p>
+                <hr>
+                <p class="bj">
+                    <span class="banji">内分泌代谢疾病基础班</span><br>
+                    <span class="xiangqing">课程内容设计糖尿病的基础治疗手段和防治诊疗、以及降糖药的选择与延展等。</span>
+                    <a><img src="/image/airclass/u42.png"></a>
+                </p>
+                <hr>
+            </div>
+            <div>
+                <a href="/kzkt/signup" class="button expanded">2016空中课堂报名</a>
+                <a href="#" class="button expanded">查看历史报名记录</a>
+
+
+            </div>
+        </div>
+
+    </div>
 
 </body>
 </html>
