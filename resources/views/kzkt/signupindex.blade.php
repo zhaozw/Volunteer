b266e562c436a329f20f776acc53d2d5b22c6cc8
amudogxxx <amudog_1@hotmail.com>
2016-02-18 10:03:34 +0800

    fix ui

--- resources/views/kzkt/signupindex.blade.php
+++ resources/views/kzkt/signupindex.blade.php
@@ -30,59 +30,58 @@
 
 </head>
 <body>
-    <div class="row" id="airclass">
-        <div class="small-10 small-centered large-4 large-centered columns">
-            <h4 class="text-center">课程简介</h4>
-            <h6 class="text-left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;全科医学协作平台远程教育项目-2016空中课堂内分泌代谢基础班、高级版、精品班由蓝海联盟（北京）医学研究院主办，由内分泌代谢专家结合基层常见的内分泌疾病诊疗误区和难点、设置的远程教育课程。</h6>
+<div class="row" id="airclass">
+    <div class="small-10 small-centered large-4 large-centered columns">
+        <h4 class="text-center">课程简介</h4>
+        <h6 class="text-left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;全科医学协作平台远程教育项目-2016空中课堂内分泌代谢基础班、高级版、精品班由蓝海联盟（北京）医学研究院主办，由内分泌代谢专家结合基层常见的内分泌疾病诊疗误区和难点、设置的远程教育课程。</h6>
+        <div>
             <div>
-                <div>
-                    <p>
-                        <img src="/image/airclass/u20.png" style="margin-left: -36px;">
-                        <span class="baoming">报名时间</span> <span class="time">2016年3月---2016年4月</span>
-                    </p>
-                    <p>
-                        <span class="baoming">开课时间</span> <span class="time">2016年5月---2016年11月</span>
-                    </p>
-                </div>
-            </div>
-            <div style="background-color: #007aff;height: 2px;width: auto"></div>
-            <div>
+                <p>
 
-                <p class="baoming"><img src="/image/airclass/u26.png">报名方式</p>
-                <a href="#" class="btn">志愿者端</a>
-                <a href="#" class="btn" style="background: #F96EA3">医生端</a>
-            </div>
-            <div>
-                <hr>
-                <p class="bj">
-                    <span class="banji">内分泌代谢疾病基础班</span><br>
-                    <span class="xiangqing">课程内容设计糖尿病的基础治疗手段和防治诊疗、以及降糖药的选择与延展等。</span>
-                    <a><img src="/image/airclass/u42.png"></a>
+                    <span class="baoming">报名时间</span> <span class="time">2016年3月---2016年4月</span>
                 </p>
-
-                <hr>
-                <p class="bj">
-                    <span class="banji">内分泌代谢疾病基础班</span><br>
-                    <span class="xiangqing">课程内容设计糖尿病的基础治疗手段和防治诊疗、以及降糖药的选择与延展等。</span>
-                    <a><img src="/image/airclass/u42.png"></a>
+                <p>
+                    <span class="baoming">开课时间</span> <span class="time">2016年5月---2016年11月</span>
                 </p>
-                <hr>
-                <p class="bj">
-                    <span class="banji">内分泌代谢疾病基础班</span><br>
-                    <span class="xiangqing">课程内容设计糖尿病的基础治疗手段和防治诊疗、以及降糖药的选择与延展等。</span>
-                    <a><img src="/image/airclass/u42.png"></a>
-                </p>
-                <hr>
-            </div>
-            <div>
-                <a href="/kzkt/signup" class="button expanded">2016空中课堂报名</a>
-                <a href="#" class="button expanded">查看历史报名记录</a>
-
-
             </div>
         </div>
+        <div style="background-color: #007aff;height: 2px;width: auto"></div>
+        <div>
 
+            <p class="baoming">报名方式</p>
+            <a href="#" class="button expanded">查看报名详情</a>
+        </div>
+        <div>
+            <hr>
+            <p class="bj">
+                <span class="banji">内分泌代谢疾病基础班</span><br>
+                <span class="xiangqing">课程内容设计糖尿病的基础治疗手段和防治诊疗方面的基础内容</span>
+                <a><img src="/image/airclass/u42.png"></a>
+            </p>
+
+            <hr>
+            <p class="bj">
+                <span class="banji">内分泌代谢疾病基础班</span><br>
+                <span class="xiangqing">课程内容围绕糖尿病并发症预防和治疗、以及降糖药的选择与延展等。</span>
+                <a><img src="/image/airclass/u42.png"></a>
+            </p>
+            <hr>
+            <p class="bj">
+                <span class="banji">内分泌代谢疾病基础班</span><br>
+                <span class="xiangqing">课程以病例分享以及课题研究的形式对内分泌常见疾病的讨论，病例谈论分析。</span>
+                <a><img src="/image/airclass/u42.png"></a>
+            </p>
+            <hr>
+        </div>
+        <div>
+            <a href="/kzkt/signup" class="button expanded">2016空中课堂报名</a>
+            <a href="#" class="button expanded">查看历史报名记录</a>
+
+
+        </div>
     </div>
+
+</div>
 
 </body>
 </html>
