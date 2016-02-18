8a80a59f3a091cc210efc560325a163cea995520
amudogxxx <amudog_1@hotmail.com>
2016-02-18 13:51:43 +0800

    fix ui

--- resources/views/kzkt/signupindex.blade.php
+++ resources/views/kzkt/signupindex.blade.php
@@ -30,55 +30,54 @@
 
 </head>
 <body>
-<div class="row" id="airclass">
-    <div class=" large-4 large-centered columns">
-        <h4 class="text-center">课程简介</h4>
-        <h6 class="text-left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;全科医学协作平台远程教育项目-2016空中课堂内分泌代谢基础班、高级版、精品班由蓝海联盟（北京）医学研究院主办，由内分泌代谢专家结合基层常见的内分泌疾病诊疗误区和难点、设置的远程教育课程。</h6>
-        <div>
+    <div class="row" id="airclass">
+        <div class=" large-4 large-centered columns">
+            <h4 class="text-center">课程简介</h4>
+            <h6 class="text-left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;全科医学协作平台远程教育项目-2016空中课堂内分泌代谢基础班、高级版、精品班由蓝海联盟（北京）医学研究院主办，由内分泌代谢专家结合基层常见的内分泌疾病诊疗误区和难点、设置的远程教育课程。</h6>
             <div>
-                <p>
-                    <span class="baoming">报名时间</span> <span class="time">2016年3月---2016年4月</span>
+                <div class="bm_sj">
+                    <p>
+                        <span class="baoming">报名时间</span> <span class="time">2016年3月---2016年4月</span>
+                    </p>
+                    <p>
+                        <span class="baoming">开课时间</span> <span class="time">2016年5月---2016年11月</span>
+                    </p>
+                </div>
+            </div>
+            <hr>
+
+            <div>
+                <p class="baoming">班级详情</p>
+                <p class="bj">
+                    <span class="banji">内分泌代谢疾病基础班</span><br>
+                    <span class="xiangqing">课程内容设计糖尿病的基础治疗手段和防治诊疗方面的基础内容</span>
+                    <a><img src="/image/airclass/u42.png"></a>
                 </p>
-                <p>
-                    <span class="baoming">开课时间</span> <span class="time">2016年5月---2016年11月</span>
+
+                <hr>
+                <p class="bj">
+                    <span class="banji">内分泌代谢疾病基础班</span><br>
+                    <span class="xiangqing">课程内容围绕糖尿病并发症预防和治疗、以及降糖药的选择与延展等。</span>
+                    <a><img src="/image/airclass/u42.png"></a>
                 </p>
+                <hr>
+                <p class="bj">
+                    <span class="banji">内分泌代谢疾病基础班</span><br>
+                    <span class="xiangqing">课程以病例分享以及课题研究的形式对内分泌常见疾病的讨论，病例谈论分析。</span>
+                    <a><img src="/image/airclass/u42.png"></a>
+                </p>
+                <hr>
+            </div>
+            <div>
+                <a href="#" class="button expanded">报名方式</a>
+                <a href="/kzkt/signup" class="button expanded">2016空中课堂报名</a>
+                <a href="#" class="button expanded">查看历史报名记录</a>
+
+
             </div>
         </div>
-        <hr>
-        <div>
-            <a href="#" class="button expanded">报名方式</a>
-        </div>
-        <div>
-            <hr>
-            <p class="bj">
-                <span class="banji">内分泌代谢疾病基础班</span><br>
-                <span class="xiangqing">课程内容设计糖尿病的基础治疗手段和防治诊疗方面的基础内容</span>
-                <a><img src="/image/airclass/u42.png"></a>
-            </p>
 
-            <hr>
-            <p class="bj">
-                <span class="banji">内分泌代谢疾病基础班</span><br>
-                <span class="xiangqing">课程内容围绕糖尿病并发症预防和治疗、以及降糖药的选择与延展等。</span>
-                <a><img src="/image/airclass/u42.png"></a>
-            </p>
-            <hr>
-            <p class="bj">
-                <span class="banji">内分泌代谢疾病基础班</span><br>
-                <span class="xiangqing">课程以病例分享以及课题研究的形式对内分泌常见疾病的讨论，病例谈论分析。</span>
-                <a><img src="/image/airclass/u42.png"></a>
-            </p>
-            <hr>
-        </div>
-        <div>
-            <a href="/kzkt/signup" class="button expanded">2016空中课堂报名</a>
-            <a href="#" class="button expanded">查看历史报名记录</a>
-
-
-        </div>
     </div>
-
-</div>
 
 </body>
 </html>
