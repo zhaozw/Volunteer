e9c7c5c6c356706b6d53a35ba852fb8b5258e657
amudogxxx <amudog_1@hotmail.com>
2016-03-02 15:52:42 +0800

    fix

--- resources/views/kzkt/signupmenu.blade.php
+++ resources/views/kzkt/signupmenu.blade.php
@@ -117,7 +117,7 @@
                     <p>手机号:{{$index['phone']}}</p>
                     <p>报名时间:{{$index['time']}}</p>
                 </a>
-                <a onclick="goViewCard({{$index['id']}})">
+                <a onclick="goViewCard({{$index['id']}})" class="span_">
                     <span>点<br>击<br>转<br>发</span>
                 </a>
             </div>
